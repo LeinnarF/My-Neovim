5dcdfcec4714b6b2700f7e6ffdf4a05fe88f6ece
Leinnarf <hilariofranniel@gmail.com>
2026-03-17 22:51:40 +0800

    fixed side marker not showing

--- lua/options.lua
+++ lua/options.lua
@@ -26,53 +26,53 @@ vim.g.vimtex_syntax_conceal_disable = 0
 
 if not vim.g.vscode then
 
-  -- Transparent statusline
-  vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
-  vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+	-- Transparent statusline
+	vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+	vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
 
-  -- Disable mode display since we use lualine
-  vim.opt.showmode = false
-  vim.opt.cmdheight = 0
+	-- Disable mode display since we use lualine
+	vim.opt.showmode = false
+	vim.opt.cmdheight = 0
 
-  -- Error lens
-  vim.diagnostic.config({
-    virtual_text = {
-      spacing = 2,
-      prefix = '',
-    },
-    severity_sort = true,
-    float = {
-      source = true,
-      border = {
-        { "┌", "FloatBorder" },
-        { "─", "FloatBorder" },
-        { "┐", "FloatBorder" },
-        { "│", "FloatBorder" },
-        { "┘", "FloatBorder" },
-        { "─", "FloatBorder" },
-        { "└", "FloatBorder" },
-        { "│", "FloatBorder" },
-      },
-    },
-    signs = {},
-    update_in_insert = false,
-  })
+	-- Error lens
+	vim.diagnostic.config({
+		virtual_text = {
+			spacing = 2,
+			prefix = '',
+		},
+		severity_sort = true,
+		float = {
+			source = true,
+			border = {
+				{ "┌", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "┐", "FloatBorder" },
+				{ "│", "FloatBorder" },
+				{ "┘", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "└", "FloatBorder" },
+				{ "│", "FloatBorder" },
+			},
+		},
+		signs = {},
+		update_in_insert = false,
+	})
 
-  -- Diagnostic signs
-  local severity = vim.diagnostic.severity
-  vim.diagnostic.config({
-    signs = {
-      text = {
-        [severity.ERROR] = " ",
-        [severity.WARN]  = " ",
-        [severity.INFO]  = " ",
-        [severity.HINT]  = " ",
-      },
-    }
-  })
+	-- Diagnostic signs
+	local severity = vim.diagnostic.severity
+	vim.diagnostic.config({
+		signs = {
+			text = {
+				[severity.ERROR] = " ",
+				[severity.WARN]  = " ",
+				[severity.INFO]  = " ",
+				[severity.HINT]  = " ",
+			},
+		}
+	})
 
-  -- Highlight groups
-  local color = "#c0caf5"
-  vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { fg = color })
+	-- Highlight groups
+	local color = "#c0caf5"
+	vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { fg = color })
 
 end
