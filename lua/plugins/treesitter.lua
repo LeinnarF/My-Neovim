84aab570503cec98a37e31f79cec05f43f4a0361
Leinnarf <hilariofranniel@gmail.com>
2026-03-21 11:46:57 +0800

    Treesitter refactor

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,12 @@ return {
 	config = function ()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua","c","vim","python"},
+			ensure_installed = {
+				"lua",
+				"c",
+				"vim",
+				"python",
+			},
 			auto_install = true,
 			ignore_install = {},
 			modules = {},
