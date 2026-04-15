89d88499a8e4ca3bd6a3d97b31d805c4c720ccc9
Leinnarf <hilariofranniel@gmail.com>
2026-04-15 15:40:16 +0800

    filetype for scripts bin

--- lua/filetype.lua
+++ lua/filetype.lua
@@ -3,5 +3,6 @@ vim.filetype.add({
 		-- Matches any preceding path, literally ".config/sway/", then any filename
 		[".*/%.config/sway/.*"] = "swayconfig",
 		[".*/%.config/conky/.*"] = "lua",
+		[".*/%.local/bin/*"] = "bash",
 	}
 })
