6861cd2a1777a1268ada6fd684afca8f8e35e7ba
Leinnarf <hilariofranniel@gmail.com>
2026-04-15 15:39:42 +0800

    code runne and  open in browser plugin removed

--- lua/plugins/open-in-browser.lua
+++
@@ -1,9 +0,0 @@
-return{
-	"tyru/open-browser.vim",
-	cond = not vim.g.vscode,
-	config = function ()
-		vim.keymap.set('n', '<leader>B', function ()
-			vim.cmd("OpenBrowser " .. vim.fn.expand("%:p"))
-		end, {desc="Open current file in browser"})
-	end,
-}
